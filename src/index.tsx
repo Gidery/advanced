0345327e358882f9a6b44c4c0235eca27bc11123
Gidery <daboka3620@gmail.com>
2022-06-30 00:46:09 +0300

    get started redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const defaultState = {
+  value: 0,
+};
+
+const reducer = (
+  state = defaultState,
+  action: { type: string; payload: number }
+) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, value: state.value + action.payload };
+
+    case 'DECREMENT':
+      return { ...state, value: state.value - action.payload };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const Home = () => {
-  return <div className="App-header">Home</div>;
+  const dispatch = useDispatch();
+  const value = useSelector((state) => state.value);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'INCREMENT', payload: 2 });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'DECREMENT', payload: 3 });
+  };
+
+  return (
+    <div className="App-header">
+      Home
+      <br />
+      <span>{`value: ${value}`}</span>
+      <button onClick={incrementHandler}>increment</button>
+      <button onClick={decrementHandler}>decrement</button>
+    </div>
+  );
 };
